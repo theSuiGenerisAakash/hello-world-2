390da55aa016ebb772ca94b864f14cb5a81723d9
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-12 13:47:26 +0530

    Test for strings with an array inside them should be treated valid: PASS

--- arraySum.js
+++ arraySum.js
@@ -1,12 +1,12 @@
 module.exports = {
   verifyVar: function(a){
-    if(a instanceof Array){
+    if(a instanceof Array || (typeof a === 'string' && JSON.parse(a) instanceof Array)){
       console.log("PASS: The variable is an Array");
       return true;
     }
     else{
       console.log("FAIL: The variable passed is not an Array, it is a " + typeof a );
-      return null;
+      return false;
     }
   }
 }
